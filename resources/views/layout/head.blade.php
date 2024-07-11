930d01f63aa6a844d8cd0eece4854d8d84faf478
Rohiqim Mahtum <mahtumrohiqim28@gmail.com>
2024-07-11 11:11:12 +0700

    first commit

---
+++ resources/views/layout/head.blade.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>CRM Admin Panel</title>
+      <!-- Favicon and touch icons -->
+      <link rel="shortcut icon" href="Minister/assets/dist/img/ico/favicon.png" type="image/x-icon">
+      <!-- Start Global Mandatory Style
+         =====================================================================-->
+      <!-- jquery-ui css -->
+      <link href="Minister/assets/plugins/jquery-ui-1.12.1/jquery-ui.min.css" rel="stylesheet" type="text/css"/>
+      <!-- Bootstrap -->
+      <link href="Minister/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css"/>
+      <!-- Bootstrap rtl -->
+      <!--<link href="Minister/assets/bootstrap-rtl/bootstrap-rtl.min.css" rel="stylesheet" type="text/css"/>-->
+      <!-- Lobipanel css -->
+      <link href="Minister/assets/plugins/lobipanel/lobipanel.min.css" rel="stylesheet" type="text/css"/>
+      <!-- Pace css -->
+      <link href="Minister/assets/plugins/pace/flash.css" rel="stylesheet" type="text/css"/>
+      <!-- Font Awesome -->
+      <!-- <link href="Minister/assets/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css"/> -->
+      <!-- Pe-icon -->
+      <link href="Minister/assets/pe-icon-7-stroke/css/pe-icon-7-stroke.css" rel="stylesheet" type="text/css"/>
+      <!-- Themify icons -->
+      <link href="Minister/assets/themify-icons/themify-icons.css" rel="stylesheet" type="text/css"/>
+      <!-- End Global Mandatory Style
+         =====================================================================-->
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css' rel='stylesheet' type='text/css' />
+    <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome-animation/0.2.1/font-awesome-animation.min.css' rel='stylesheet' />
+    <link href='https://cdn.jsdelivr.net/npm/pixeden-stroke-7-icon@1.2.3/pe-icon-7-stroke/dist/pe-icon-7-stroke.min.css' rel='stylesheet' type='text/css' />
+    <link href='https://cdn.jsdelivr.net/themify-icons/0.1.2/css/themify-icons.css' rel='stylesheet' type='text/css' />
+      <!-- Start page Label Plugins 
+         =====================================================================-->
+      <!-- Emojionearea -->
+      <link href="Minister/assets/plugins/emojionearea/emojionearea.min.css" rel="stylesheet" type="text/css"/>
+      <!-- Monthly css -->
+      <link href="Minister/assets/plugins/monthly/monthly.css" rel="stylesheet" type="text/css"/>
+      <!-- End page Label Plugins 
+         =====================================================================-->
+      <!-- Start Theme Layout Style
+         =====================================================================-->
+      <!-- Theme style -->
+      <link href="Minister/assets/dist/css/stylecrm.css" rel="stylesheet" type="text/css"/>
+      <!-- Theme style rtl -->
+      <!--<link href="Minister/assets/dist/css/stylecrm-rtl.css" rel="stylesheet" type="text/css"/>-->
+      <!-- End Theme Layout Style
+         =====================================================================-->
+   </head>
+   <body class="hold-transition sidebar-mini">
+    <!--preloader-->
+    <div id="preloader">
+       <div id="status"></div>
+    </div>
---
+++ resources/views/layout/sidebar.blade.php
@@ -0,0 +1,64 @@
+         <!-- =============================================== -->
+         <!-- Left side column. contains the sidebar -->
+         <aside class="main-sidebar">
+          <!-- sidebar -->
+          <div class="sidebar">
+             <!-- sidebar menu -->
+             <ul class="sidebar-menu">
+                <li class="active">
+                   <a href="index.html"><i class="fa fa-tachometer"></i><span>Dashboard</span>
+                   <span class="pull-right-container">
+                   </span>
+                   </a>
+                </li>
+                <li>
+                   <a href="user.html">
+                   <i class="fa fa-envelope"></i><span>Surat Masuk</span>
+                   <span class="pull-right-container">
+                   </span>
+                   </a>
+                </li>
+                <li>
+                   <a href="items.html">
+                   <i class="fa fa-envelope-open-o"></i><span>Surat keluar</span>
+                   <span class="pull-right-container">
+                   </span>
+                   </a>
+                </li>
+                <li>
+                   <a href="department.html">
+                   <i class="fa fa-edit"></i><span>Disposisi Surat</span>
+                   <span class="pull-right-container">
+                   </span>
+                   </a>
+                </li>
+                <li class="treeview">
+                  <a href="#">
+                  <i class="fa fa-file-text"></i><span>Laporan</span>
+                  <span class="pull-right-container">
+                  <i class="fa fa-angle-left pull-right"></i>
+                  </span>
+                  </a>
+                  <ul class="treeview-menu">
+                     <li><a href="add-customer.html">Surat Masuk</a></li>
+                     <li><a href="clist.html">Surat Keluar</a></li>
+                     <li><a href="group.html">Kegiatan</a></li>
+                  </ul>
+               </li>
+             </ul>
+          </div>
+          <!-- /.sidebar -->
+       </aside>
+       <!-- =============================================== -->
+       <!-- Content Wrapper. Contains page content -->
+       <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <div class="header-icon">
+              <i class="fa fa-dashboard"></i>
+            </div>
+            <div class="header-title">
+              <h1>SI SURAT</h1>
+              <small>Pusat Sistem Informasi dan Multimedia <span>(PUSIM)</span> Universitas Merdeka Malang</small>
+            </div>
+        </section>
